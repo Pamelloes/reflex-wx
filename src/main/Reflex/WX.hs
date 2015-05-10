345a07ef8de37318a1ea58a06c69de2c51695a4a
Pamelloes <pamelloes@gmail.com>
2015-05-10 16:04:46 -0500

    Initial commit. Created a new cabal project with license and dependencies.
    Signed-off-by: Pamelloes <pamelloes@gmail.com>

---
+++ src/main/Reflex/WX.hs
@@ -0,0 +1,10 @@
+{-|
+Module      : Reflex.WX
+Description : The root module for reflex-wx. This will import all necessary
+              modules for normal use of reflex-wx.
+License     : wxWindows Library License
+Maintainer  : joshuabrot@gmail.com
+Stability   : Experimental
+-}
+module Reflex.WX (
+                 ) where
