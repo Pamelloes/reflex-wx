83691cb73be8408cc6db7cbec95898c9e45a2db4
Pamelloes <pamelloes@gmail.com>
2015-06-16 17:44:59 -0500

    Updated example to reflect new api and stuff.
    Signed-off-by: Pamelloes <pamelloes@gmail.com>

--- src/test/Main.hs
+++ src/test/Main.hs
@@ -10,34 +10,17 @@ module Main where
 
 import Data.Monoid
 
-import qualified Graphics.UI.WX as W
-
 import Reflex.WX
 
---program :: (MonadComponent t m) => m ()
+--program :: (MonadWidget t m) => m ()
 program = do 
   frame [text := "Test Application"] $ do
-    rec b <- button [text := "Click"]
-        cmd <- command b
-        d1 <- mapDyn (("Presses: " ++).show) =<< count (mergeWith (<>) [cmd,c])
-        d2 <- mapDyn (("Presses: " ++).show) =<< count (mergeWith (<>) [cmd,d])
-        t <- label [text :~ d1]
-        s <- label [text :~ d2]
-        (_,(c,d)) <- panel [] $ do
-                   setLayout (W.column 10)
-                   b1 <- button [text := "Click me!"]
-                   b2 <- button [text := "Me, too!"]
-                   c1 <- command b1
-                   c2 <- command b2
-                   d1 <- command b1
-                   d2 <- command b2
-                   return (mergeWith (<>) [c1,c2], mergeWith (<>) [d1,d2])
-        e <- entry [ text := "Maybe" ]
-    t1 <- get text t
-    t2 <- get text e
-    label [text :~ t1]
-    label [text :~ t2]
-    return ()
+    t <- textCtrl []
+    b <- button [ text := "Click" ]
+    return $ column 0 [ minsize (sz 250 50) $ widget t
+                      , glue
+                      , widget b
+                      ]
   return ()
 
 main :: IO ()
